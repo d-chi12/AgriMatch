23f035e297b213e024931e5808b2ce10fe769766
USER <k.k.fliper12@gmail.com>
2020-07-29 23:52:21 +0900

    add intro text

--- app/views/intro/index.html.rb
+++ app/views/intro/index.html.rb
@@ -17,3 +17,61 @@
           </ul>
         </div>
 </section>
+
+<section class="p-usage" id="usage">
+        <div class="l-container">
+          <h2 class="p-usage__title">ご利用方法</h2>
+          <ul class="p-usage__tab-button__wrapper">
+            <li class="p-usage__tab-button js-tabButton active" data-target=".js-tabItem--producer">生産者</li>
+            <li class="p-usage__tab-button js-tabButton" data-target=".js-tabItem--worker">働き手</li>
+          </ul>
+          <div class="p-usage--producer js-tabItem js-tabItem--producer container" style="display: flex;">
+            <div class="swiper-container gallery-top mobile swiper-container-horizontal" style="cursor: grab;">
+              <div class="swiper-wrapper" style="transition-duration: 300ms; transform: translate3d(-2961px, 0px, 0px);"><div class="swiper-slide swiper-slide-duplicate" style="background-image: url(&quot;assets/images/slide_producer_01.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="0"></div><div class="swiper-slide swiper-slide-duplicate" style="background-image: url(&quot;assets/images/slide_producer_02.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="1"></div><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-prev" style="background-image: url(&quot;assets/images/slide_producer_03.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="2"></div><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active" style="background-image: url(&quot;assets/images/slide_producer_04.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="3"></div>
+                <div class="swiper-slide swiper-slide-duplicate-next" style="background-image: url(&quot;assets/images/slide_producer_01.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="0"></div>
+                <div class="swiper-slide" style="background-image: url(&quot;assets/images/slide_producer_02.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="1"></div>
+                <div class="swiper-slide swiper-slide-prev" style="background-image: url(&quot;assets/images/slide_producer_03.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="2"></div>
+                <div class="swiper-slide swiper-slide-active" style="background-image: url(&quot;assets/images/slide_producer_04.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="3"></div>
+              <div class="swiper-slide swiper-slide-duplicate swiper-slide-next" style="background-image: url(&quot;assets/images/slide_producer_01.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="0"></div><div class="swiper-slide swiper-slide-duplicate" style="background-image: url(&quot;assets/images/slide_producer_02.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="1"></div><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-prev" style="background-image: url(&quot;assets/images/slide_producer_03.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="2"></div><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active" style="background-image: url(&quot;assets/images/slide_producer_04.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="3"></div></div>
+            <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
+            <div class="swiper-container gallery-thumbs swiper-container-vertical swiper-container-free-mode swiper-container-thumbs">
+              <div class="swiper-wrapper" style="transition-duration: 300ms; transform: translate3d(0px, 0px, 0px);">
+                <div class="swiper-slide swiper-no-swipng swiper-slide-visible swiper-slide-active" style="height: 112px;">アカウントの登録</div>
+                <div class="swiper-slide swiper-no-swipng swiper-slide-visible swiper-slide-next" style="height: 112px;">案件を登録する</div>
+                <div class="swiper-slide swiper-no-swipng swiper-slide-visible" style="height: 112px;">チャットでやりとり</div>
+                <div class="swiper-slide swiper-no-swipng swiper-slide-visible swiper-slide-thumb-active" style="height: 112px;">終わったあとはレビューで評価</div>
+              </div>
+            <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
+          </div>
+          <div class="p-usage--worker js-tabItem js-tabItem--worker container" style="display: none;">
+            <div class="swiper-container gallery-top--worker mobile swiper-container-horizontal" style="cursor: grab;">
+              <div class="swiper-wrapper" style="transition-duration: 0ms; transform: translate3d(-1692px, 0px, 0px);"><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active" style="background-image: url(&quot;assets/images/slide_worker_01.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="0"></div><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-next" style="background-image: url(&quot;assets/images/slide_worker_02.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="1"></div><div class="swiper-slide swiper-slide-duplicate" style="background-image: url(&quot;assets/images/slide_worker_03.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="2"></div><div class="swiper-slide swiper-slide-duplicate swiper-slide-prev" style="background-image: url(&quot;assets/images/slide_worker_04.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="3"></div>
+                <div class="swiper-slide swiper-slide-active" style="background-image: url(&quot;assets/images/slide_worker_01.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="0"></div>
+                <div class="swiper-slide swiper-slide-next" style="background-image: url(&quot;assets/images/slide_worker_02.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="1"></div>
+                <div class="swiper-slide" style="background-image: url(&quot;assets/images/slide_worker_03.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="2"></div>
+                <div class="swiper-slide swiper-slide-duplicate-prev" style="background-image: url(&quot;assets/images/slide_worker_04.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="3"></div>
+              <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active" style="background-image: url(&quot;assets/images/slide_worker_01.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="0"></div><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-next" style="background-image: url(&quot;assets/images/slide_worker_02.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="1"></div><div class="swiper-slide swiper-slide-duplicate" style="background-image: url(&quot;assets/images/slide_worker_03.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="2"></div><div class="swiper-slide swiper-slide-duplicate" style="background-image: url(&quot;assets/images/slide_worker_04.png&quot;); width: 393px; margin-right: 30px;" data-swiper-slide-index="3"></div></div>
+            <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
+            <div class="swiper-container gallery-thumbs--worker swiper-container-vertical swiper-container-free-mode swiper-container-thumbs">
+              <div class="swiper-wrapper" style="transition-duration: 0ms; transform: translate3d(0px, 0px, 0px);">
+                <div class="swiper-slide swiper-no-swipng swiper-slide-thumb-active swiper-slide-visible swiper-slide-active" style="height: 112px;">アカウントの登録</div>
+                <div class="swiper-slide swiper-no-swipng swiper-slide-visible swiper-slide-next" style="height: 112px;">プロフィールを登録する</div>
+                <div class="swiper-slide swiper-no-swipng swiper-slide-visible" style="height: 112px;">チャットでやりとり</div>
+                <div class="swiper-slide swiper-no-swipng swiper-slide-visible" style="height: 112px;">終わったあとはレビューで評価</div>
+              </div>
+            <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
+          </div>
+        </div>
+        <div class="p-usage--example">
+          <div class="l-container">
+            <h3 class="p-usage--example__title">扱う仕事の例</h3>
+            <ul class="p-usage--example__task">
+              <li class="p-usage--example__item p-usage--example__item--above"><img class="p-usage--example__item__image" src="assets/images/img_kouten.png" alt="耕耘アイコン">耕耘</li>
+              <li class="p-usage--example__item p-usage--example__item--above"><img class="p-usage--example__item__image" src="assets/images/img_sehi.png" alt="施肥アイコン">施肥</li>
+              <li class="p-usage--example__item p-usage--example__item--under"><img class="p-usage--example__item__image" src="assets/images/img_tanemaki.png" alt="種まきアイコン">種まき</li>
+              <li class="p-usage--example__item p-usage--example__item--under"><img class="p-usage--example__item__image" src="assets/images/img_nae.png" alt="苗植えアイコン">苗植え</li>
+              <li class="p-usage--example__item p-usage--example__item--under"><img class="p-usage--example__item__image" src="assets/images/img_saibai.png" alt="栽培管理アイコン">栽培管理</li>
+            </ul>
+          </div>
+        </div>
+</section>
