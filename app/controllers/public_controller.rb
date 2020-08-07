35d6d8552be9d7f6a6eaf1403e30f7ac95b6bb80
USER <k.k.fliper12@gmail.com>
2020-08-07 23:54:24 +0900

    fix language

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,7 +2,7 @@ class PublicController < ApplicationController
   def main
     if account_signed_in?
       path = current_account.admin? ? accounts_path : dashboard_path
-      redirect_to dashboard_path, flas: { success: "Successfully singed in. Welcome to Property Finder!" } and return
+      redirect_to dashboard_path, flas: { success: "サインインに成功しました。AgriMatchへようこそ！" } and return
     end
 
     @properties = Property.latest
