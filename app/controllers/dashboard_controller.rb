ce33c3a4c873e21100a1fc58254a7573accd7e9e
USER <k.k.fliper12@gmail.com>
2020-08-11 18:27:40 +0900

    profile add ブログ　リンク

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,6 +7,8 @@ class DashboardController < ApplicationController
   def profile
     @account = Account.find(params[:id])
     @properties = Property.where(account_id: @account.id)
+    @posts = Post.all
+
 
     @properties_sold = Property.where(account_id: @account.id).sold.count
     @properties_for_sale = Property.where(account_id: @account.id).for_sale.count
